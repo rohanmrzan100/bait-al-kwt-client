b5b49e7b643ee1384918c54a7770adec89e50d16
Rohan Maharjan <rohanmrzan100@gmail.com>
2024-07-21 21:30:07 +0545

    fix

--- Constants.ts
+++ Constants.ts
@@ -3,7 +3,7 @@ let BASE_URL: string;
 if (process.env.NODE_ENV !== "production") {
   BASE_URL = "http://localhost:5000";
 } else {
-  BASE_URL = process.env.BASE_URL || "http://ns3.actnepal.com.np:9000";
+  BASE_URL = process.env.BASE_URL || "https://ns3.actnepal.com.np:9000";
 }
 
 export { BASE_URL };
