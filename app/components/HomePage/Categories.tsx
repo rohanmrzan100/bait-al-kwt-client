862edfb9fa146455be612dd63217f08b8b6e3e1f
Rohan Maharjan <rohanmrzan100@gmail.com>
2024-07-22 07:53:48 +0545

    fix

--- app/components/HomePage/Categories.tsx
+++ app/components/HomePage/Categories.tsx
@@ -8,7 +8,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 
-const Categories = () => {
+const CategorySlider = () => {
   const router = useRouter();
   const settings = {
     dots: true,
@@ -19,55 +19,34 @@ const Categories = () => {
   };
 
   return (
-<<<<<<< HEAD
     <div className="my-[16px]">
       <h1 className="text-[22px] text-slate-800 font-semibold mb-[16px]">
         Categories
       </h1>
-      <div className="grid grid-cols-9 md:grid-cols-9 gap-1">
+      <Slider {...settings}>
         {Categorys.map((item) => {
           return (
             <div
               key={item.label}
               onClick={() => {
-                if (item.label == "All") {
+                if (item.label === "All") {
                   router.push("/shop");
                 } else {
                   router.push("/shop?category=" + item.label);
                 }
               }}
-              className=" bg-white border-[1px] border-[#e2e2e2] flex flex-col  gap-8 transition cursor-pointer hover:shadow-xl min-w-[150px] justify-center items-center p-8 rounded-xl shadow-lg"
+              className="bg-white border-[1px] border-[#e2e2e2] flex flex-col gap-8 transition cursor-pointer hover:shadow-xl min-w-[150px] justify-center items-center p-8 rounded-xl shadow-lg"
             >
               <item.icon className="p-0 w-[40px] h-[40px]" />
               <div className="font-medium text-sm text-center overflow-hidden p-0">
                 {item.label}
-=======
-    <div className="w-full m-auto cursor-pointer">
-      <div className="mt-20 text-center">
-        <h2 className="text-3xl font-semibold mb-6">Categories</h2>
-        <Slider {...settings}>
-          {Categorys.map((item) => {
-            return (
-              <div key={item.label} className="h-60">
-                <div className="flex flex-col justify-between items-center text-center bg-white w-full p-4 gap-3 hover:scale-110 shadow-lg h-full ">
-                  <img
-                    src={item.image.src}
-                    alt={`${item.label} image`}
-                    className="max-h-[100px] object-contain "
-                  />
-
-                  <p className="text-xl font-semibold m-0 p-0">
-                    {item.label}
-                  </p>
-                </div>
->>>>>>> b5b49e7b643ee1384918c54a7770adec89e50d16
               </div>
-            );
-          })}
-        </Slider>
-      </div>
+            </div>
+          );
+        })}
+      </Slider>
     </div>
   );
 };
 
-export default Categories;
+export default CategorySlider;
